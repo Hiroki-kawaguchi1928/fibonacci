acdf195b6b9170a87249c82217c96a1049aa88fc
川口博己 <h.kawaguchi1928@gmail.com>
2025-06-04 20:51:56 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 'use strict';
+const memo = new Map() //連想配列の作成
+memo.set(0, 0); //データの登録（key, value)
+memo.set(1, 1); //データの登録（key, value）
+
+
 function fib(n) {
-  if (n === 0) {
-    return 0;
-  }else if (n === 1){
-    return 1;
-  }
-  return fib(n - 1) + fib(n - 2);
+  if (memo.has(n)) return memo.get(n)
+  //データを持っていないとき
+  const value = fib(n - 1) + fib(n - 2);
+  memo.set(n, value); //データの登録（key, value)
+  return value;　//値を返す
 }
-const length = 40;
+const length = 100;
 for (let i= 0; i <= length; i++) {
   console.log(fib(i));
 }
